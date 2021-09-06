c5e0a3e569f6314ca2e8e5797a2313217e42f944
Rob Hamilton <robertkhamilton@users.noreply.github.com>
2021-09-06 16:07:53 -0400

    adding simple audio examples, proximity, contact sounds, collision

---
+++ Unity/Simple 3d Scene/Assets/Scripts/CrashSound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashSound : MonoBehaviour {
+
+	public AudioClip crashSoft;
+	public AudioClip crashHard;
+
+	private AudioSource source;
+	private float lowPitchRange = .15F;
+	private float highPitchRange = 1.75F;
+	private float velToVol = .2F;
+	private float velocityClipSplit = 10F;
+
+
+	void Awake () {
+		source = GetComponent<AudioSource> ();
+
+	}
+
+	void  OnCollisionEnter (Collision coll) {
+
+		source.pitch = Random.Range (lowPitchRange, highPitchRange);
+
+		float hitVol = coll.relativeVelocity.magnitude * velToVol * 0.1f;
+
+		//source.PlayOneShot (crashSoft, 1F);
+
+		if (coll.relativeVelocity.magnitude < velocityClipSplit) {
+			//source.PlayOneShot (crashSoft, 1F);
+			source.PlayOneShot (crashSoft, hitVol);
+		} else {
+			//source.PlayOneShot (crashHard, 1F);
+			source.PlayOneShot (crashHard, hitVol);
+		}
+	}
+}
---
+++ Unity/Simple 3d Scene/Assets/Scripts/ThrowObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ThrowObject : MonoBehaviour {
+
+	public GameObject projectile;
+	public AudioClip shootSound;
+
+	private float throwSpeed = 2000f;
+	private AudioSource source;
+
+	private float volLowRange = 0.5f;
+	private float volHighRange = 1.0f;
+
+	void Awake () {
+		source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if (Input.GetButtonDown ("Fire1")) {
+		
+		//if(Mouse.current.leftButton.isPressed)
+		if ( Input.GetMouseButtonDown(0) )
+		{
+			float vol = Random.Range (volLowRange, volHighRange);
+			source.PlayOneShot (shootSound, vol);
+
+			GameObject throwThis = Instantiate (projectile, transform.position, transform.rotation) as GameObject;
+			throwThis.GetComponent<Rigidbody>().AddRelativeForce (new Vector3(0,0,throwSpeed));
+		}
+
+	}
+}
